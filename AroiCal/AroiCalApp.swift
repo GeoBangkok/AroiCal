7a5ccee3c4a7c3a4f8bb5efa7d3c5a21198295e2
GeoBangkok <42850725+GeoBangkok@users.noreply.github.com>
2026-02-26 03:10:08 -0600

    Initial Commit

---
+++ AroiCal/AroiCalApp.swift
@@ -0,0 +1,32 @@
+//
+//  AroiCalApp.swift
+//  AroiCal
+//
+//  Created by standard on 2/26/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AroiCalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
